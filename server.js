b739059bd5ea2b6447ccf1bc1207f74be62caf3e
santi <santimartinezalbert02@gmail.com>
2022-12-05 21:43:38 +0100

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
-    res.json({ "message": "IIIEEEPAAA que pasa gente" });
+    res.json({ "message": "IIIEEPAAA que pasa gente" });
 });
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
