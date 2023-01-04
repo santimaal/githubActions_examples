b39938994b24cf15d83b186f6008a716a829f7a9
santi <santimartinezalbert02@gmail.com>
2023-01-04 12:52:19 +0100

    eror actions/core

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const core = require('actions/core');
+const core = require('@actions/core');
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
