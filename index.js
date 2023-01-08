72aed687cf9f852744f6c3c5887db66af96acf09
santi <santimartinezalbert02@gmail.com>
2023-01-08 17:27:43 +0100

    no se

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 try {
-  const token = process.env.telegram_token
   // const token = core.getInput('telegram_token');
-  const chat_id = core.getInput('chat_id');
+  console.log("aaa")
+  console.log(core.getInput('telegram_token'))
+  // const chat_id = core.getInput('chat_id');
   const message = "this is a test"
   
   // Create a bot that uses 'polling' to fetch new updates
-  const bot = new TelegramBot(token, {polling: true});
+  // const bot = new TelegramBot(token, {polling: true});
   
-  bot.sendMessage(chat_id, message);
+  // bot.sendMessage(chat_id, message);
 }
 catch (e){
   core.setFailed(e.message)
