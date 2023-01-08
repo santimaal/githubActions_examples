fc1d43b9dac0740152046137a3eb25544d330b6e
santi <santimartinezalbert02@gmail.com>
2023-01-08 17:11:56 +0100

    action changed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.telegram_token
+const token = process.env.TELEGRAM_TOKEN
 
-const chat_id = Number(process.env.telegram_chat);
+const chat_id = Number(process.env.TELEGRAM_CHAT);
 const message = "this is a test"
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -13,24 +13,24 @@ const bot = new TelegramBot(token, {polling: true});
 bot.sendMessage(chat_id, message);
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  console.log(msg);
+// // Listen for any kind of message. There are different kinds of
+// // messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   console.log(msg);
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, msg.text);
-});
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, msg.text);
+// });
